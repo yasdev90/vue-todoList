f9671372e0e74d3546f6c2af65c60a0036a5eb9b
Yasser Dlewati <yasdle@outlook.com>
2020-07-16 16:18:11 +0800

    got tasks from api, added navigation controls

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="app">
     <Header @add-todo="addTodo"/>
-    <TodosList :todos="todosList" />
+    <TodosList :todos="displayedTodos" :currentPage="pageIndex" :totalPages="todosList.length/itemsPerPage" @prev-page="prevPage" @next-page="nextPage" @page-size-changed="setPageSize" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Header from './components/header.vue'
 import TodosList from './components/todos-list.vue'
 
@@ -15,38 +16,57 @@ export default {
     Header,
     TodosList
   },
+  props:["pageSize"],
   data(){
     return{
-      todosList:[
-        {
-          id:1,
-          title:'develop',
-          completed: false,
-        },
-        {
-          id:2,
-          title:'test',
-          completed: true,
-        },
-        {
-          id:3,
-          title:'deploy',
-          completed: false,
-        },
-      ]
+      todosList:[],
+      displayedTodos:[],
+      pageIndex : 1,
+      itemsPerPage:10,
+      lastItemIndex :0,
     }
   },
   methods:{
     addTodo(newTodo){
       this.todosList.push(newTodo)
+    },
+    nextPage:function (){    
+      if(this.pageIndex<this.todosList.length/this.itemsPerPage) {
+      this.pageIndex ++;
+      this.lastItemIndex += parseInt(this.itemsPerPage)
+      this.displayTodos()
+      }
+    },
+    prevPage:function (){
+      if(this.pageIndex>1){
+      this.pageIndex --;
+      this.lastItemIndex -= parseInt(this.itemsPerPage)
+       this.displayTodos()
+      }
+    }, 
+    displayTodos: function(){
+      this.displayedTodos = this.todosList.slice(this.lastItemIndex, parseInt(this.lastItemIndex) + parseInt(this.itemsPerPage))
+    },
+    setPageSize: function(pageSize){
+     this.itemsPerPage = pageSize
+     this.lastItemIndex = 0
+     this.pageIndex = 1
+     this.displayTodos()
     }
-  }
+  },
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos').then(res=>{
+      this.todosList = res.data
+      this.displayTodos()
+      })   
+  }  
 }
 </script>
 
 <style>
 body{
   margin:0;
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
 }
 
 </style>
